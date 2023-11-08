bb191c43c17546c46a808ed3ff87744126f86bda
Hailey <137557225+officiallyhailey@users.noreply.github.com>
2023-11-08 04:03:16 -0500

    mock portal

--- login.js
+++ login.js
@@ -1,11 +1,37 @@
-$(document).ready(function() {
-    $('#form').on('submit', function(e) {
-        e.preventDefault();
-        var password = $('#wholesale763').val();
-        if (password === 'correctpassword') {
-            window.open('https://shop.silkcitycoffee.com', '_blank');
-        } else {
-            alert('Incorrect password. Please try again.');
-        }
-    });
-});
+let go = document.getElementById('submit');
+let password = document.getElementById('passwordField');
+let error = document.querySelector('.error');
+let revealButton = document.querySelector('.reveal')
+
+
+function checkPassword() {
+
+    if (passwordField.value === "wholesale763") {
+        window.location.href='https://shop.silkcitycoffee.com/collections/coffee'
+      
+    } else {
+
+        document.querySelector('.error').innerHTML = 'Try again!';
+        error.classList.add('error-fade')
+        setTimeout(() => {
+            error.innerHTML = ''
+            error.classList.remove('error-fade')
+          }, 2000);
+        
+    }
+}
+
+revealButton.addEventListener('click', function() {
+
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      revealButton.classList.remove('fa-eye-slash')
+      revealButton.classList.add('fa-eye')
+    } else {
+      passwordField.type = "password";
+      revealButton.classList.add('fa-eye-slash')
+      revealButton.classList.remove('fa-eye') 
+    }
+ })
+         
+ 
